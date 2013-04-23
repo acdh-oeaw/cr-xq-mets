295330165aeda2dbe645170798b36e1cd2fbb9f8
vronk <tech@vronk.net>
2013-04-23 12:00:17 +0200

    adding webscripts (css, js) as shared-module

--- src/modules/shared/scripts/js/query_input/qi.js
+++ src/modules/shared/scripts/js/query_input/qi.js
@@ -1,3 +1,7 @@
+
+// jQuery closure 
+(function($) {
+
 /**
  * @class QueryInput
  * generate a customizable query_input UI, a input form with fields/widgets based on settings
@@ -9,14 +13,8 @@
  * @author vronk, Andy Basch
  * @version 2013-01-17
  */
-
-
 /* we could make it a class (instead of jQuery plugin)
-       function QueryInput(elem,s) */ 
-       
-// jQuery closure 
-(function($) {
-
+       function QueryInput(elem,s) */     
 $.fn.QueryInput = function (options)
 {
    /** the dom-element to generate the query input in */
@@ -27,7 +25,7 @@ $.fn.QueryInput = function (options)
       */
    var settings = $.extend(true, {}, defaults, options);
     blendInParams(settings.params, getUrlParams(location.search))  
-  
+   
    // makes the settings publicly available as .data("qi")
    elem.data("qi",settings); 
    init(settings);   
@@ -52,7 +50,7 @@ $.fn.QueryInput = function (options)
          } else if (trg_container)  {
             var label= param.label ? param.label : key;
             var new_input_label = param.label=='' ? '' : "<label>" + label+ "</label>";
-            var new_input, new_widget=null;
+            var new_input=null, new_widget=null;
             
             switch (param.widget) {
                 case "text":
@@ -67,6 +65,14 @@ $.fn.QueryInput = function (options)
                 case "autocomplete":
                   new_input = genAutocomplete (key, param);    
                   break;
+               case "cql":
+                  var cql_elems = genCQLInput(key, param);
+                  new_input = cql_elems[0];
+                  new_widget = cql_elems[1]; 
+                  break;
+                case "link":
+                  new_input = genLink (key, param);    
+                  break;
                 case "slider":
                   var slider_elems = genSlider(key, param);
                   new_input = slider_elems[0];
@@ -75,28 +81,35 @@ $.fn.QueryInput = function (options)
                 default:
                   console.log("no such widget: " + param.widget); 
               }    
-                  
-         // set initial value
-         $(new_input).val(param.value)
-                     .data("key", key)
-                     .attr("id", settings.input_prefix + key)
-                     .data("param-object", param);
-             
-        /* update settings and widgets upon value-change */
-        new_input.change(function () {
-                   setParamValue(this);
-             });
-                   
-            $(form).append(new_input_label, new_input, new_widget);
-           // inputs.push(key, new_input_label, new_input, new_widget]);
-            
+        
+          if (new_input) {
+            $(new_input).data("key", key)
+                        .addClass("type-" + param.widget)
+                             .attr("id", settings.input_prefix + key)
+                             .data("param-object", param);
+                     
+            if (param.widget!="link") {
+                 // set initial valuelink
+                 $(new_input).val(param.value);
+                     
+                /* update settings and widgets upon value-change */
+                new_input.change(function () {
+                           setParamValue(this);
+                     });
+               }            
+               
+             $(form).append(new_input_label, new_input, new_widget);
+            // inputs.push(key, new_input_label, new_input, new_widget]);
+           }
          }
-       }   
+       } // end for
+       
+       
     }
 /*
     function formatForm(inputs) {
       for ( var key in inputs ) {
-      
+    
       }
         <table>
         </table>
@@ -110,6 +123,18 @@ $.fn.QueryInput = function (options)
         return input;
     }
     
+    function genLink(key, param_settings) {
+         
+        var a = $("<a>" + key + "</a>");
+        a.attr("href", generateLink());
+       
+            $(a).mousedown(function(e) {
+                $(this).attr("target", "_blank");
+                $(this).attr("href", generateLink());
+         });
+        return a;
+    }
+    
     function genSubmit(key, param_settings) {
          
         var input = $("<input type='submit'/>");
@@ -118,12 +143,31 @@ $.fn.QueryInput = function (options)
         return input;
     }
         
-    /** generating out own comboboxes, because very annoying trying to use some of existing jquery plugins (easyui.combo, combobox, jquery-ui.autocomplete) */ 
+    /** generating our own comboboxes, because very annoying trying to use some of existing jquery plugins (easyui.combo, combobox, jquery-ui.autocomplete) */ 
     function genCombo (key, param_settings) {
     
-        var select = $("<select id='widget-" + key + "' />")
-            select.attr("id", settings.input_prefix + key)
-        param_settings.values.forEach(function(v) { $(select).append("<option value='" + v +"' >" + v + "</option>") });
+        var select = $("<select id='widget-" + key + "' />");
+            //select.attr("id", settings.input_prefix + key)
+            
+       if (param_settings.static_source) {
+              //var scanURL = settings.fcs_source +  param_settings.index
+              var source_url = param_settings.static_source.replace(/&amp;/g,'&');
+              // if static source - try to retrieve the data 
+              $.getJSON(source_url, function(data) {
+                    param_settings.values = data.terms
+                    param_settings.values.forEach(function(v) { $(select).append("<option value='" + v.value +"' >" + v.label + "</option>") });
+                    //console.log($(input).autocomplete().source);
+              });
+        
+             //param_settings.source = fcsScan;
+        } else if (param_settings.values) {
+            //    $(input).autocomplete(param_settings);
+            param_settings.values.forEach(function(v) { $(select).append("<option value='" + v +"' >" + v + "</option>") });
+        } else { /* if no values,  rather make a textbox out of it? */ 
+          //select = 
+        }
+        
+        select.attr("id", settings.input_prefix + key)
         return select;
     }
 
@@ -132,25 +176,26 @@ $.fn.QueryInput = function (options)
         
         var input = $("<input />");
          $(input).attr("name",key)
-        
+   //     console.log(key, param_settings.static_source);
         if (param_settings.static_source) {
               //var scanURL = settings.fcs_source +  param_settings.index
-              var scanURL = param_settings.static_source.replace(/&amp;/g,'&');
+              var source_url = param_settings.static_source.replace(/&amp;/g,'&');
               // if static source - try to retrieve the data 
-              $.getJSON(scanURL, function(data) {
+              $.getJSON(source_url, function(data) {
                     param_settings.source = data.terms
                     $(input).autocomplete(param_settings);
-                  //  console.log($(input).autocomplete("option","source").length);
+                    //console.log($(input).autocomplete().source);
               });
         
              //param_settings.source = fcsScan;
         } else {
-            $(input).autocomplete(param_settings);
+        $(input).autocomplete(param_settings);
         }
          
         return input;
     }
 
+    
     function fcsScan(request, response) {
         response( $.ui.autocomplete.filter(
                           scan.terms, request.term ) );
@@ -169,8 +214,8 @@ $.fn.QueryInput = function (options)
       
         var new_widget = $("<div class='widget-" + param_settings.widget + "'></div>");
             new_widget.attr("id", "widget-" + key)
-                         .css(settings.slider_style)
-                         .slider( param_settings)
+                      .css(settings.slider_style)
+                      .slider( param_settings)
                 
                         // set both-ways references between the input-field and its slider - necessary for updating 
                         .data("related-input-field",new_input)
@@ -196,7 +241,8 @@ $.fn.QueryInput = function (options)
         accepting settings as reliable source of current value
         i.e. changes in input have to change the .value in settings.
         This is assured by calling setParamValue upon input-change.
-        @public by calling: $(elem).data("qi").getParamValue(param_key);
+        public by calling: $(elem).data("qi").getParamValue(param_key);
+        @public 
     */
     settings.getParamValue = function(key) {
          if (this.params[key])  {
@@ -206,6 +252,17 @@ $.fn.QueryInput = function (options)
          }
     }
 
+    /** allow to update the current value in settings from outside */
+    settings.setParamValue = function(key, value) {
+        
+        if (settings.params[key]) {
+            settings.params[key].value = value
+        }
+        // callback:
+        settings.onValueChanged.call( settings,value );
+        return value    
+    }
+    
     /** update the current value in settings */
     function setParamValue(input_object) {
         var param_object = $(input_object).data("param-object");
@@ -217,7 +274,7 @@ $.fn.QueryInput = function (options)
         settings.onValueChanged.call( input_object,value );
         return value    
     }
-
+    
     
     /** get params from the uri */
     function getUrlParams(url)
@@ -249,6 +306,20 @@ $.fn.QueryInput = function (options)
        for ( var key in settings_params ) {
             if (params[key]) {settings_params[key].value=params[key]; }      
        }
+    }
+
+ /** generate a query-link out of the current values of the params */
+    function generateLink()
+    {
+      var url="?";
+      
+       for ( var key in settings.params) {
+           if (settings.params[key].value) {
+                 url += key + "=" + settings.params[key].value + "&"; 
+             }      
+       }
+       
+      return url;
     }
 
 
